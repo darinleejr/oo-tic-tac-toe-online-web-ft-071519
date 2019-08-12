e9e62a716c12566ccd601f8edf806a26381ec219
Darin Webster <darinleenavy@gmail.com>
2019-08-12 20:33:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,6 +2,10 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
+  
+  def WIN_COMBINATIONS
+    
+  end
  
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
